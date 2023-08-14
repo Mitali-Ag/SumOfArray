329799e8d71f71c5b18d9bd39b4970ed8d09370d
Mitali Agrawal <141202178+Mitali-Ag@users.noreply.github.com>
2023-08-14 18:51:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRO_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {10,20, 30,40,50};
+            int sum = 0;
+ 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("Sum of all the elements of an array: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
